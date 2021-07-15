4f95354ff8aa2a59cf139d972858210f6088619f
poliveira13 <poliveira13@hotmail.com>
2021-07-15 16:49:18 -0300

    page singup

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import SignIn from './pages/SingIn';
+import SignUp from './pages/SingUp';
 import GlobalStyle from './styles/global';
 import Routes from './routes';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <SignUp />
     <GlobalStyle />
   </>
 );
--- src/pages/SingUp/index.tsx
+++ src/pages/SingUp/index.tsx
@@ -1,12 +1,37 @@
-import React from "react";
+import React from 'react';
+import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import logoimg from '../../assets/logo.svg';
 
+import Button from '../../components/Button';
+import Input from '../../components/Input';
 
-import {Title} from "./styles"
+import { Container, Background, Content } from './styles';
 
-const SignUp: React.FC = () =>{
-  return(
-    <Title>Olá signIn</Title>
-  )
-}
+const SignUp: React.FC = () => {
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoimg} alt="Go Barber" />
+        <form>
+          <h1>Faça seu cadastro</h1>
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+          <Button type="submit">Cadastrar</Button>
+        </form>
+        <a href="login">
+          <FiArrowLeft />
+          Voltar para Logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
 
 export default SignUp;
