17c3f4a90b5b3d8ba904193cc1b36fd6a69ae07c
poliveira13 <poliveira13@hotmail.com>
2021-07-16 10:26:07 -0300

    unform e atualizacao do react correcao bug

--- src/pages/SingUp/index.tsx
+++ src/pages/SingUp/index.tsx
@@ -2,18 +2,23 @@ import React from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import logoimg from '../../assets/logo.svg';
 
+import { Form } from '@unform/web';
+
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 
 import { Container, Background, Content } from './styles';
 
 const SignUp: React.FC = () => {
+  function handleSubmit(data: object) {
+    console.log('aaaaa');
+  }
   return (
     <Container>
       <Background />
       <Content>
         <img src={logoimg} alt="Go Barber" />
-        <form>
+        <Form onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
           <Input name="name" icon={FiUser} placeholder="Nome" />
           <Input name="email" icon={FiMail} placeholder="E-mail" />
@@ -24,7 +29,7 @@ const SignUp: React.FC = () => {
             placeholder="Senha"
           />
           <Button type="submit">Cadastrar</Button>
-        </form>
+        </Form>
         <a href="login">
           <FiArrowLeft />
           Voltar para Logon
